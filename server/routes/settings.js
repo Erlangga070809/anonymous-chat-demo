dee0f864c793e0b5d07bde0ed7e08f7ed300ca4f
Erlangga070809 <sstokmek@gmail.com>
2026-08-14 04:12:01 +0700

    Create settings.js

---
+++ server/routes/settings.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const settingsController = require('../controllers/settingsController');
+const { authenticate } = require('../middleware/auth');
+
+router.get('/', authenticate, settingsController.getSettings);
+router.patch('/', authenticate, settingsController.updateSettings);
+
+module.exports = router;
