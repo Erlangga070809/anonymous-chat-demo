b1d8a7b5f8de16c8fd63397bdfab8c4e92b225e2
Erlangga070809 <sstokmek@gmail.com>
2026-08-14 04:11:51 +0700

    Create reports.js

---
+++ server/routes/reports.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const reportController = require('../controllers/reportController');
+const { authenticate } = require('../middleware/auth');
+
+router.post('/', authenticate, reportController.createReport);
+router.get('/mine', authenticate, reportController.getMyReports);
+
+module.exports = router;
